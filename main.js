27835139e93fa03a35c242c2717c5260d0a63658
CodeSonia <sonia_ah@icloud.com>
2021-08-30 15:36:30 +0000

    reordered js code

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 /*XMLHttpRequest object is an inbuilt obj that JS provides to allow us to consume APIs. This gives us the method to open connections, to send connections and to close connections */
 let xhr = new XMLHttpRequest();
 
+//Open a connection:
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+
+xhr.send();
+
 //Listener waiting for the state to change
 xhr.onreadystatechange = function() {
   if(this.readyState == 4 && this.status == 200) {
     document.getElementById("data").innerHTML = this.responseText;
   }
 };
-
-//Open a connection:
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-
-xhr.send();

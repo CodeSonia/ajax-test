da1c39064a2e1e100db8aaae983f5c8e3b17c276
CodeSonia <sonia_ah@icloud.com>
2021-08-30 16:43:26 +0000

    created callback function as another alternative

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-let xhr = new XMLHttpRequest();
-let data;
+function getData(cb) {
+  let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+  xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+  xhr.send();
 
-xhr.onreadystatechange = function() {
-  if(this.readyState == 4 && this.status == 200) {
-    data = JSON.parse(this.responseText);
-  }
-};
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
+    }
+  };
+}
 
-// setTimeout function takes 2 parameters. The first is a callback function. The second is the milliseconds to wait for the function to do its thing. 
-
-//Get our code to wait on execution
-setTimeout(function() {
+function printDataToConsole(data) {
   console.log(data);
-}, 500);
+}
 
+getData(printDataToConsole);

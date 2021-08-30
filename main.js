f3bf40254466fb903ac67716a74b345f086c26b6
CodeSonia <sonia_ah@icloud.com>
2021-08-30 16:37:04 +0000

    added comment explaining one way how to get code to wait on execution

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ xhr.onreadystatechange = function() {
   }
 };
 
-// setTimeout functiom
+// setTimeout function takes 2 parameters. The first is a callback function. The second is the milliseconds to wait for the function to do its thing. 
+
+//Get our code to wait on execution
 setTimeout(function() {
   console.log(data);
 }, 500);

281868068b64d23b8dcd892d0d6587e472cd8dd3
CodeSonia <sonia_ah@icloud.com>
2021-08-30 16:36:11 +0000

    created setTimeout function

--- main.js
+++ main.js
@@ -2,18 +2,16 @@ let xhr = new XMLHttpRequest();
 let data;
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-
 xhr.send();
-
-function setData(jsonData) {
-  data = jsonData;
-}
 
 xhr.onreadystatechange = function() {
   if(this.readyState == 4 && this.status == 200) {
-    setData(JSON.parse(this.responseText));
+    data = JSON.parse(this.responseText);
   }
 };
 
-console.log(data);
+// setTimeout functiom
+setTimeout(function() {
+  console.log(data);
+}, 500);
 
